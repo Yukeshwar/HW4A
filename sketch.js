724f42f5daee9f066289db9fcb143822b404264b
Yukeshwar <123797754+Yukeshwar@users.noreply.github.com>
2024-10-03 00:26:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,21 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); 
+  background(220, 220, 220); 
+  let centerX = width / 2; 
+  let centerY = height / 2; 
+  let numCircles = 8; 
+  let ringSpacing = 100; 
+  for (let r = 1; r <= 5; r++) { 
+    for (let i = 0; i < numCircles; i++) { 
+      let angle = (TWO_PI / numCircles) * i; 
+      let x = centerX + cos(angle) * r * ringSpacing; 
+      let y = centerY + sin(angle) * r * ringSpacing; 
+      let circleSize = r * 10; 
+      fill(50 + r * 40, 100 + r * 20, 200 - r * 30); 
+      noStroke(); 
+      ellipse(x, y, circleSize, circleSize);
+    }
+  }
 }
 
 function draw() {
